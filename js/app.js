7c68ebbfbf1f29193212d95ff01451b14ce47007
EnesKarapinar <karapinarabdullahenes@gmail.com>
2023-01-27 16:50:05 +0300

    coming soon added

--- js/app.js
+++
@@ -1,8 +0,0 @@
-//Header
-$(window).scroll(function() {
-    if ($(window).scrollTop() >= 0 && $(window).scrollTop() <= 500) {
-        $(".fix-header").removeClass("change")
-    } else {
-        $(".fix-header").addClass("change")
-    }
-})
